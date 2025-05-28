c1a7525f539f22b1cfb6922a7b7c51d168271a65
Dmitriy Marchenko <98233694+DmitrMarch@users.noreply.github.com>
2025-05-28 20:41:07 +0300

    add the README and dashboard in json format

--- SystemMonitoring/obj/Debug/net8.0/SystemMonitoring.AssemblyInfo.cs
+++ SystemMonitoring/obj/Debug/net8.0/SystemMonitoring.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemMonitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0760d08e2f18e403840b84456a49bbc4f64f65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5436432b0c151609079704cb257ca62cf45865")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemMonitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemMonitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
